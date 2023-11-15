ec4a9df4450506e3c0cf2de2abc6de3fe774fe3b
vsafonova <viktoriyablak@gmail.com>
2023-11-15 23:29:35 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+async function getData() {
+  const url = "https://api.imgflip.com/get_memes";
+  const options = {
+    method: "GET",
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    return result.data.memes;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function main() {
+  const data = await getData();
+  showRandomMeme(data);
+
+  let memeButton = document.getElementById("memeButton");
+  memeButton.addEventListener("click", function () {
+    showRandomMeme(data);
+  });
+}
+
+main();
+
+function getRandomEl(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function showRandomMeme(memeList) {
+  let randomMeme = getRandomEl(memeList);
+    let imageMemeEl = document.getElementById("memeImage");
+    imageMemeEl.src = randomMeme.url;
+}
